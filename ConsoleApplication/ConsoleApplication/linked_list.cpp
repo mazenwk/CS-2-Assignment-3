a82a88fd0355c533e4b81472600150f549c30488
mazenwkamel-auc <mazenwkamel@aucegypt.edu>
2022-10-16 20:43:24 +0200

    Implemented Vector part of assignment

---
+++ ConsoleApplication/ConsoleApplication/linked_list.cpp
@@ -0,0 +1,18 @@
+#include "linked_list.h"
+
+template <typename T> struct node {
+private:
+	T data{};
+	int data_count{};
+
+public:
+	node(T data)
+	{
+
+	}
+	node* next;
+};
+
+template <typename T> class linked_list {
+
+};
--- ConsoleApplication/ConsoleApplication/main.cpp
+++ ConsoleApplication/ConsoleApplication/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <vector>
+
+
+void insert_after(std::vector<int>& vector, int first_element, int second_element);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n{};
+    std::cout << "Input numeber of integers to input: "; std::cin >> n;
+    std::vector<int> numbers;
+
+    int input{};
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << "Input number " << i+1 << ": "; std::cin >> input;
+        numbers.push_back(input);
+    }
+
+    int first_element{}, second_element{};
+    std::cout << "Input first element to search for: "; std::cin >> first_element;
+    std::cout << "Input second element to insert: "; std::cin >> second_element;
+
+    insert_after(numbers, first_element, second_element);
+}
+
+void insert_after(std::vector<int>& vector, int first_element, int second_element) {
+    std::vector<int>::iterator iter{};
+    
+    for (iter = vector.begin(); iter < vector.end(); iter++)
+    {
+        if (*iter == first_element) {
+            vector.insert(iter + 1, second_element);
+            return;
+        }
+    }
 }
