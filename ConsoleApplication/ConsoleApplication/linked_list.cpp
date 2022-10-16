b6e0716faa3d0df1778450afb1e5b8fa166c3edd
mazenwkamel <107046923+mazenwk@users.noreply.github.com>
2022-10-16 22:49:35 +0200

    Implemented Linked List

--- ConsoleApplication/ConsoleApplication/linked_list.cpp
+++ ConsoleApplication/ConsoleApplication/linked_list.cpp
@@ -1,18 +1,4 @@
 #include "linked_list.h"
 
-template <typename T> struct node {
-private:
-	T data{};
-	int data_count{};
 
-public:
-	node(T data)
-	{
 
-	}
-	node* next;
-};
-
-template <typename T> class linked_list {
-
-};
--- ConsoleApplication/ConsoleApplication/main.cpp
+++ ConsoleApplication/ConsoleApplication/main.cpp
@@ -1,19 +1,24 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include "linked_list.h"
 #include <iostream>
 #include <vector>
 
 
 void insert_after(std::vector<int>& vector, int first_element, int second_element);
+void print_vector(const std::vector<int>& vector);
 
-int main()
-{
+void test_vector() {
     int n{};
-    std::cout << "Input numeber of integers to input: "; std::cin >> n;
+    std::cout << "Input number of integers to input: "; std::cin >> n;
     std::vector<int> numbers;
 
     int input{};
-    for (size_t i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        std::cout << "Input number " << i+1 << ": "; std::cin >> input;
+        std::cout << "Input number " << i + 1 << ": "; std::cin >> input;
         numbers.push_back(input);
     }
 
@@ -22,16 +27,71 @@ int main()
     std::cout << "Input second element to insert: "; std::cin >> second_element;
 
     insert_after(numbers, first_element, second_element);
+    print_vector(numbers);
 }
 
-void insert_after(std::vector<int>& vector, int first_element, int second_element) {
-    std::vector<int>::iterator iter{};
-    
-    for (iter = vector.begin(); iter < vector.end(); iter++)
+void test_linked_list() {
+    linked_list<int> numbers;
+    numbers.print_elements();
+    numbers.push(1);
+    numbers.print_elements();
+
+    numbers.push(3);
+    numbers.push(87);
+    numbers.print_elements();
+
+    numbers.pop();
+    numbers.print_elements();
+
+}
+
+void test_linked_list_2() {
+    int n{};
+    std::cout << "Input number of integers to input: "; std::cin >> n;
+    std::vector<int> vector;
+
+    int input{};
+    for (int i = 0; i < n; i++)
     {
-        if (*iter == first_element) {
-            vector.insert(iter + 1, second_element);
-            return;
+        std::cout << "Input number " << i + 1 << ": "; std::cin >> input;
+        vector.push_back(input);
+    }
+
+    int first_element{}, second_element{};
+    std::cout << "Input first element to search for: "; std::cin >> first_element;
+    std::cout << "Input second element to insert: "; std::cin >> second_element;
+
+    insert_after(vector, first_element, second_element);
+    print_vector(vector);
+
+    linked_list<int> numbers(vector);
+    numbers.print_elements();
+}
+
+int main()
+{
+    //test_vector();
+    //test_linked_list();
+    test_linked_list_2();
+
+    _CrtDumpMemoryLeaks();
+    return 0;
+}
+
+void insert_after(std::vector<int>& vector, const int first_element, const int second_element) {
+
+	for (auto itr = vector.begin(); itr < vector.end(); ++itr)
+    {
+        if (*itr == first_element) {
+            vector.insert(itr + 1, second_element);
+            break;
         }
     }
 }
+
+void print_vector(const std::vector<int>& vector) {
+	for (std::size_t i = 0; i < vector.size(); ++i) {
+        std::cout << "Element number [" << i + 1 << "] is: " << vector[i] << '\n';
+	}
+}
+
